1dc1a9fad089b171e47e855f14d40a84468bd63e
kirandash <thekirankumardash@gmail.com>
2024-11-30 17:24:05 +0800

    1 Eslint and prettier

--- my-first-next-app/src/app/page.tsx
+++ my-first-next-app/src/app/page.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
 import Image from 'next/image';
 
+import Calendar from 'react-calendar';
+
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <Calendar className="row-start-1" />
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <Image
           className="dark:invert"
